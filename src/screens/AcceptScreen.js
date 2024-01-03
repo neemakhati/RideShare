8cc4ad06b699378ab813c974aa059fcd65f46f1d
neemakhati <neemakhati11@gmail.com>
2024-01-03 11:34:00 +0545

    TESTing Commit KRAKEN

--- src/screens/AcceptScreen.js
+++ src/screens/AcceptScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
-
+//TESTING COMMIT 
 const AcceptScreen = () => {
   const info = useSelector(state => state.info);
 
